247d87d746cab96fbfc1adf2468d9db384aafc16
bengquist <engquist_blake@yahoo.com>
2019-06-09 23:37:55 -0500

    add mongo db addon

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
 const url =
+  process.env.MONGODB_URI ||
   "mongodb+srv://bengquist:yourmom1023@reservations-cjvgc.mongodb.net/test?retryWrites=true&w=majority";
 
 mongoose.connect(url, { useNewUrlParser: true });
